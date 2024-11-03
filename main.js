4dcb0ae23af3ca8262bc251f4d7073b361be51bb
Ulad L <ulad@Ulads-Air-M2.lan>
2024-11-03 10:47:49 -0600

    final

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-const button = document.querySelector('.button0');
-const menu = document.querySelector('.menu1')
-const menuLinks = document.querySelectorAll('.menu__link')
-
-button.addEventListener('click', () =>{
-    button.classList.toggle('active')
-
-    if(button.classList.contains('.active')){
-        button.setAttribute('aira-expanded', 'true')
-        menu.setAttribute('aira-hidden', 'false')
-        menuLinks.forEach(link => link.setAttribute('tabindex', '0'))
-    }else{
-        button.setAttribute('aira-expanded', 'false')
-        menu.setAttribute('aira-hidden', 'true')
-        menuLinks.forEach(link => link.setAttribute('tabindex', '-1'))
-    }
-});
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+document.querySelector("header").classList.toggle("open")
+    })
+})
